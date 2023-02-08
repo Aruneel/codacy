f19f77cf574d489437e8308bb475f52d0c2975de
Aruneel Das <aruneel.das@arcadix.com>
2023-02-08 19:10:44 +0530

    Test

---
+++ src/Test.js
@@ -0,0 +1,16 @@
+function Test() {
+    let strArr = ["a", "b", "c"];
+
+    return (
+        <div>
+            <h1>Test</h1>
+            <ul>
+                {strArr.map((str, index) => {
+                    return <li>{str}</li>
+                })}         
+            </ul>
+        </div>
+    );
+}
+
+export default App;
