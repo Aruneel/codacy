f64d1c2b222469f6d117289dfa6e7e6f35e8a958
Aruneel Das <aruneel.das@arcadix.com>
2023-02-08 19:14:31 +0530

    .

--- src/Test.js
+++ src/Test.js
@@ -1,5 +1,5 @@
 function Test() {
-    let strArr = ["a", "b", "c"];
+    let strArr = ["a", "b", "c"]
 
     return (
         <div>
@@ -10,7 +10,7 @@ function Test() {
                 })}         
             </ul>
         </div>
-    );
+    )
 }
 
-export default Test;
+export default Test
